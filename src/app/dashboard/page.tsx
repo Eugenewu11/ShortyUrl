e5cf678c8623e691fa7676de983e81a213e16f7b
Eugenewu11 <genewu013@gmail.com>
2025-11-13 18:53:33 -0600

    03.3: Aplicacion terminada, por implementar: funcionalidad de verificacion de correo con Resend, funcionalidad en reset de contraseña en caso de olvido

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -99,7 +99,7 @@ export default function DashboardPage() {
       {/*Header*/}
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-          <h1 className="text-2xl font-bold text-gray-900">🔗 Acortador de Links</h1>
+          <h1 className="text-2xl font-bold text-gray-900">Acortador de Links TinyURL</h1>
           <button
             onClick={handleLogout}
             className="px-4 py-2 text-sm font-medium text-white bg-red-600 hover:bg-red-700 rounded-lg transition-colors shadow-md hover:shadow-lg"
